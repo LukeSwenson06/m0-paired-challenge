60da96347c376146ec010a88cd4d866c07aad81d
Luke Swenson <LukeSwenson95@outlook.com>
2022-02-09 20:35:35 -0700

    Complete challenge

---
+++ datatype.rb
@@ -0,0 +1,7 @@
+#Chose this name because it involves data types
+library = "I like them" #This is a string
+number_of_books = 5  #This is a integer
+fines = 10.00 # This is a float
+fine_paid = true # This is a boolean
+
+favorite_books = ["lord of the rings", "harry potter", "beloved"]
---
+++ index.md
@@ -0,0 +1,10 @@
+#Great Books
+1. The Great Gatsby
+2. Lord of the Rings
+3. Harry Potter
+4. The hobbit
+5. Beloved
+
+#Workflow for this challenge
+Deannah was the navigator and Luke was the driver. We both collborating, shouting,
+having input and laughing while completing
